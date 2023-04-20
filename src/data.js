b8ea449e6a2089e9cf8c64667bd4bb57c773129b
snikolica <nikola.stevanovic2011@gmail.com>
2023-04-20 18:25:55 +0200

    first-commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'Nikola Stevanovic',
+        job: 'web developer',
+        image: 'https://tse1.mm.bing.net/th?id=OIP.cOIhmdMaEVjEuKe33B9n9wHaF7&pid=Api&P=0',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing.'
+    },
+    {
+        id: 2,
+        name: 'Stasa Stevanovic',
+        job: 'farmaceut',
+        image: 'https://tse1.mm.bing.net/th?id=OIP.sVUjnSWT61bAL4kE8JblMgHaE8&pid=Api&P=0',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum aliquid sapiente, expedita reprehenderit nobis quidem!'
+    },
+    {
+        id: 3,
+        name: 'Marko Stevanovic',
+        job: 'farmaceut',
+        image: 'https://tse1.mm.bing.net/th?id=OIP.Cr57_6ohSa8PnE5AApADtQHaE7&pid=Api&P=0',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum '
+    },
+    {
+        id: 4,
+        name: 'Vanja Milosevic',
+        job: 'fizioterapeut',
+        image: 'https://tse4.mm.bing.net/th?id=OIP.TTXfm_3t3gH3bsNaqVBYbAHaFK&pid=Api&P=0',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum aliquid sapiente, expedita '
+    }
+
+];
+export default reviews;
